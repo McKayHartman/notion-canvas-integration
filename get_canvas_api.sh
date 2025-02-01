b77618bf50d08efb75c97b1a79ab0726ed7a8de3
McKay Hartman <mckay@hwsrv-1244326.hostwindsdns.com>
2025-02-01 02:24:15 +0000

    updated shell script to get data

--- get_canvas_api.sh
+++ get_canvas_api.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
 # base url
-BASE_URL="https://nau.instructure.com/api/v1"
+BASE_URL="https://canvas.nau.com/api/v1"
 
 # api key
 API_KEY="19664~a67Un3MaRf2T7VCJ7hQDzr2uwP9rfPuzvFN6CCNaYxch4UuQf6rk4ZAYNTnuahk7"
 
 #course codes
-course_codes=("35851" "35935" "35850" "35859" "35867" "34795" "36223")
+CODES=("35851" "35935" "35850" "35859" "35867" "34795" "36223")
 
-#fetch assignment data
+#output
+output_file="fetched_assignment_data.json"
+
+#clear file
+echo "[" > "$output_file"
+
+#get assignement data
 fetch_assignments(){
-for code in course_code;
-do
-    RESPONSE=$(curl -s -H "Authorization: Bearer $API_KEY" "$BASE_URL/$course")
-    echo "$RESPONSE"
-done 
+for code in "${CODES[@]}"
+    do
+	echo "$code info"
+        RESPONSE=$(curl -s -X GET -H "Authorization: Bearer $API_KEY" "https://canvas.nau.edu/api/v1/courses/$code/assignments")
+        echo "$RESPONSE" >> "$output_file"
+
+	if [ "$course_id" != "${CODES[-1]}" ] && [ -n "$course_id" ]; then
+	    echo "." >> "$output_file"
+	fi
+    done
+
+echo "]" >> "$output_file"
+
+echo "Data saved to $output_file"
+}
+fetch_assignments 
