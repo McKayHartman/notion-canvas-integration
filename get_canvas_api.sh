d998307c732195a9ce338fe414e7b5fd69db1a64
McKay Hartman <mckay@hwsrv-1244326.hostwindsdns.com>
2025-02-06 19:35:29 +0000

    fixed bash script formatting the json file incorrectly and added features to the python script to add a url to the updated data

--- get_canvas_api.sh
+++ get_canvas_api.sh
@@ -17,18 +17,24 @@ echo "[" > "$output_file"
 
 #get assignement data
 fetch_assignments(){
+local first=true
+
 for code in "${CODES[@]}"
     do
 	echo "$code info"
         RESPONSE=$(curl -s -X GET -H "Authorization: Bearer $API_KEY" "https://canvas.nau.edu/api/v1/courses/$code/assignments")
-        echo "$RESPONSE" >> "$output_file"
+	OBJECT=$(echo "$RESPONSE" | jq -c --arg course_id "$code" '.[] | . + {course_id: $course_id}')
 
-	if [ "$course_id" != "${CODES[-1]}" ] && [ -n "$course_id" ]; then
-	    echo "." >> "$output_file"
-	fi
-    done
+	while IFS= read -r line; do
+	    if [ "$first" = false ]; then
+	        echo "," >> "$output_file"
+	    fi
+	    echo "$line"  >> "$output_file"
+	    first=false
+        done <<< "$OBJECT"
+done
 
-echo "]" >> "$output_file"
+echo -e  "\n]" >> "$output_file"
 
 echo "Data saved to $output_file"
 }
---
+++ parse_assignments.py
@@ -0,0 +1,32 @@
+# Libraries
+import json
+
+# set the base url
+BASE_URL = "https://canvas.nau.edu/courses"
+# open fetched asignment data json file
+with open('fetched_assignment_data.json', 'r') as file:
+    new_data = json.load(file)
+
+# Initalize an array to hold the updated data
+updated_data = []
+
+# Iterate over the raw data from the fetched json file 
+
+for assignment in new_data:
+	# put into object "extracted data"
+	extracted_data = {
+	    'assignment_id': assignment.get('id'),
+	    'course_id': assignment.get('course_id'),
+	    'name': assignment.get('name'),
+	    'due_at': assignment.get('due_at'),
+	    'points_possible': assignment.get('points_possible'),
+	    'score': assignment.get('submission', {}).get('score', None),
+	    'url': f"{BASE_URL}/{assignment.get('course_id')}/assignments/{assignment.get('id')}" if assignment.get('course_id') and assignment.get('id') else None
+
+	}
+	
+	# append it to the updated data
+	updated_data.append(extracted_data)
+print("Updated JSON data")
+print(json.dumps(updated_data, indent=4))
+	
